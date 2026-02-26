5e49f37824aaa9e352ea400cda684583ccae8541
kanishk799 <25113cn050@glbitm.ac.in>
2026-02-26 16:13:30 +0530

    Create To swap the two integers.c

---
+++ To swap the two integers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+   int x, y;
+   printf("Enter first number : ");
+   scanf("%d" , &x);
+
+   printf("Enter second number : ");
+   scanf("%d" , &y);
+
+   printf("\nBefore swapping : x = %d, y = %d" , x ,y);
+   swap(&x ,&y);
+   
+   printf("\nAfter swapping : x = %d, y = %d\n , x, y");
+   return 0;
+}
+
