e99c5d4b66cdc0d6ed2413628e5669e40499eabb
kanishk799 <25113cn050@glbitm.ac.in>
2026-02-26 16:10:03 +0530

    Create two no are equal or not.c

---
+++ two no are equal or not.c
@@ -0,0 +1,18 @@
+// WAP to check number is equal or not
+#include <stdio.h>
+int main()
+  
+{
+   int a, b;
+  scanf("%d %d",&a,&b);
+   if (a==b)
+   {
+    printf("equal");    
+   }
+   else 
+   {
+    printf("not equal");
+   }
+   return 0;
+   }
+    
